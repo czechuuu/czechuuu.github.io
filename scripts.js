651d3fb5cafba8c77f606beba8a3704b11464583
czechuuu <czechuuu@users.noreply.github.com>
2020-08-07 11:36:01 +0200

    alerts
    alerts

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ request.send();
 
 request.onload = function() {
     const jsonData = request.response;
+    alert("onload!")
     loadingData(jsonData)
 }
 
@@ -17,7 +18,7 @@ function loadingData(jsonObj) {
     document.getElementById('piatek').innerHTML = jsonObj['piatek'];
     document.getElementById('sobota').innerHTML = jsonObj['sobota'];
     document.getElementById('niedziela').innerHTML = jsonObj['niedziela'];
-
+    alert("loading data!")
     document.getElementById('poniedzialek').style.backgroundColor = jsonObj['c_poniedzialek'];
     document.getElementById('wtorek').style.backgroundColor = jsonObj['c_wtorek'];
     document.getElementById('sroda').style.backgroundColor = jsonObj['c_sroda'];
