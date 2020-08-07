a572019eb03ee0fb71f819dbe76b6f652367b5ac
czechuuu <czechuuu@users.noreply.github.com>
2020-08-07 11:20:41 +0200

    dodanie json p.1

--- scripts.js
+++ scripts.js
@@ -1,4 +1,33 @@
+let requestURL = 'czechuuu.github.io/data.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    const jsonData = request.response;
+    loadingData(jsonData)
+}
+
+function loadingData(jsonObj) {
+    document.getElementById('poniedzialek').innerHTML = jsonObj['poniedzialek'];
+    document.getElementById('wtorek').innerHTML = jsonObj['wtorek'];
+    document.getElementById('sroda').innerHTML = jsonObj['sroda'];
+    document.getElementById('czwartek').innerHTML = jsonObj['czwartek'];
+    document.getElementById('piatek').innerHTML = jsonObj['piatek'];
+    document.getElementById('sobota').innerHTML = jsonObj['sobota'];
+    document.getElementById('niedziela').innerHTML = jsonObj['niedziela'];
+
+    document.getElementById('poniedzialek').style.backgroundColor = jsonObj['c_poniedzialek'];
+    document.getElementById('wtorek').style.backgroundColor = jsonObj['c_wtorek'];
+    document.getElementById('sroda').style.backgroundColor = jsonObj['c_sroda'];
+    document.getElementById('czwartek').style.backgroundColor = jsonObj['c_czwartek'];
+    document.getElementById('piatek').style.backgroundColor = jsonObj['c_piatek'];
+    document.getElementById('sobota').style.backgroundColor = jsonObj['c_sobota'];
+    document.getElementById('niedziela').style.backgroundColor = jsonObj['c_niedziela'];
+}
+
 function buttonPress(id, text, color) {
-  document.getElementById(id).innerHTML = text;
-  document.getElementById(id).style.backgroundColor = color;
+    document.getElementById(id).innerHTML = text;
+    document.getElementById(id).style.backgroundColor = color;
 }
